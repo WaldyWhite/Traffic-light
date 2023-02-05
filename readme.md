a92bf4b2346f24c78e946f1f87e44bb35f9ba207
WaldyWhite <w.weis@hotmail.com>
2023-02-05 15:25:28 +0100

    added new

---
+++ readme.md
@@ -0,0 +1,1 @@
+This task has only been solved with document.querySelector, addEventListener and removeEventListener. A more modified traffic light is in the task task-6.8.1 — Стартовая страница.
---
+++ script.js
@@ -0,0 +1,57 @@
+const trafLightRed = document.querySelector('#lightRed');
+const trafLightYellow = document.querySelector('#lightYellow');
+const trafLightGreen = document.querySelector('#lightGreen');
+
+
+trafLightRed.addEventListener('click', makeRed);
+function makeRed() {
+    trafLightRed.style.background = ('red');
+    trafLightYellow.style.background = ('black');
+    trafLightGreen.style.background = ('black');
+
+    // Listener remove
+    trafLightRed.removeEventListener('click', makeRed);
+    trafLightYellow.removeEventListener('click', makeRed);
+    trafLightGreen.removeEventListener('click', makeRed);
+
+    // Listener set
+    trafLightRed.addEventListener('click', makeYellow);
+    trafLightYellow.addEventListener('click', makeYellow);
+    trafLightGreen.addEventListener('click', makeYellow);   
+}
+
+trafLightYellow.addEventListener('click', makeYellow);
+function makeYellow() {
+   
+    trafLightYellow.style.background = ('yellow');
+    trafLightRed.style.background = ('black');
+    trafLightGreen.style.background = ('black');
+
+    // Listener remove
+    trafLightRed.removeEventListener('click', makeYellow);
+    trafLightYellow.removeEventListener('click', makeYellow);
+    trafLightGreen.removeEventListener('click', makeYellow);
+
+    // Listener set
+    trafLightRed.addEventListener('click', makeGreen);
+    trafLightYellow.addEventListener('click', makeGreen);
+    trafLightGreen.addEventListener('click', makeGreen);
+}
+
+trafLightGreen.addEventListener('click', makeGreen);
+function makeGreen() {
+    trafLightGreen.style.background = ('green');
+    trafLightRed.style.background = ('black');
+    trafLightYellow.style.background = ('black');
+
+    // Listener remove
+    trafLightRed.removeEventListener('click', makeGreen);
+    trafLightYellow.removeEventListener('click', makeGreen);
+    trafLightGreen.removeEventListener('click', makeGreen);
+
+    // Listener set
+
+    trafLightYellow.addEventListener('click', makeRed);
+    trafLightRed.addEventListener('click', makeRed);
+    trafLightGreen.addEventListener('click', makeRed);
+}
